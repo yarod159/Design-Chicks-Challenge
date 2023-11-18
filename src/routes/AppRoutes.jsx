33e9fb5b7d63d03807b512f594a47c477ee434d6
yarod159 <86536012+yarod159@users.noreply.github.com>
2023-11-17 21:25:56 -0400

    folder organization

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home/Home";
+
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<Home/>} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
